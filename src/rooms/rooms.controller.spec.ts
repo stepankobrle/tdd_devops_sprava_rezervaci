68442a5a258c346d962f6c67c7652cd869450db3
stepankobrle <152393182+stepankobrle@users.noreply.github.com>
2026-03-04 13:06:44 +0100

    implementace rest controlerů + zavislosti v controller.spec

--- src/rooms/rooms.controller.spec.ts
+++ src/rooms/rooms.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RoomsController } from './rooms.controller';
+import { RoomsService } from './rooms.service';
 
 describe('RoomsController', () => {
   let controller: RoomsController;
@@ -7,6 +8,12 @@ describe('RoomsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RoomsController],
+      providers: [
+        {
+          provide: RoomsService,
+          useValue: { createRoom: jest.fn() },
+        },
+      ],
     }).compile();
 
     controller = module.get<RoomsController>(RoomsController);
