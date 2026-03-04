a38776288db5416c9bc95fb94b4209385cc4a2b8
stepankobrle <152393182+stepankobrle@users.noreply.github.com>
2026-03-04 13:01:41 +0100

    implementace rest controlerů

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { IsEmail, IsEnum, IsOptional, IsString } from 'class-validator';
+import { UsersService } from './users.service';
+import { User, UserRole } from './user.entity';
+
+class CreateUserBody {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsEnum(UserRole)
+  role?: UserRole;
+}
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  createUser(@Body() body: CreateUserBody): Promise<User> {
+    return this.usersService.createUser(body);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User, UserRole } from './user.entity';
+
+export interface CreateUserDto {
+  name: string;
+  email: string;
+  role?: UserRole;
+}
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async createUser(dto: CreateUserDto): Promise<User> {
+    const user = this.userRepository.create(dto);
+    return this.userRepository.save(user);
+  }
+}
